87210b05f1ab13921a33302ae735b927b1ad7091
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-04-13 17:01:17 -0400

    created connection

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1286Sometimes!',
+    database: 'employee_db'
+})
