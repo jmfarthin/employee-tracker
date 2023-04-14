415aac213cf27c07cc746d9287542722784d07f9
jmfarthin <122315957+jmfarthin@users.noreply.github.com>
2023-04-14 11:03:03 -0400

    set up schema, connected database, getting an error on start up

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,25 @@
 -- create database
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    dept_id INT,
+    FOREIGN KEY (dept_id) REFERENCES department(id) ON DELETE
+    SET NULL
+);
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE
+    SET NULL
+);
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
+const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
+require('dotenv').config()
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1286Sometimes!',
-    database: 'employee_db'
-})
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.USER,
+        password: process.env.PASS,
+        database: process.env.DATABASE
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
